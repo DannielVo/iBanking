c07fd7ae47fbf487b30de0aabb737f2d9a2d12d2
DannielVo <phattinhoc2017@gmail.com>
2025-09-11 19:54:06 +0700

    Fix: Update fav-icon

--- iBanking/src/pages/login/Login.jsx
+++ iBanking/src/pages/login/Login.jsx
@@ -3,23 +3,23 @@ import "./login.css";
 
 const Login = () => {
   return (
-    <div class="login-wrapper">
-      <div class="login-container">
-        <h2 class="login-title">Log in</h2>
+    <div className="login-wrapper">
+      <div className="login-container">
+        <h2 className="login-title">Log in</h2>
 
         <form>
-          <div class="login-form-group">
-            <label for="">Username</label>
+          <div className="login-form-group">
+            <label htmlFor="">Username</label>
             <input type="text" name="" id="" />
           </div>
 
-          <div class="login-form-group">
-            <label for="">Password</label>
+          <div className="login-form-group">
+            <label htmlFor="">Password</label>
             <input type="password" name="" id="" />
           </div>
 
-          <div class="login-form-footer">
-            <button class="login-btn">Log in</button>
+          <div className="login-form-footer">
+            <button className="login-btn">Log in</button>
           </div>
         </form>
       </div>
