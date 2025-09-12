d51071c64a83dbe465b8566544a3caa8517dfd4a
DannielVo <phattinhoc2017@gmail.com>
2025-09-12 09:48:11 +0700

    Feat: Finish Profile page

--- iBanking/src/App.jsx
+++ iBanking/src/App.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import Login from "./pages/login/Login";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Profile from "./pages/profile/Profile";
 
 const App = () => {
-  return <>{/* <Login></Login> */}</>;
+  return (
+    <>
+      <Login></Login>
+      {/* <Profile></Profile> */}
+    </>
+  );
 };
 
 export default App;
---
+++ iBanking/src/pages/profile/Profile.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import "./profile.css";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+const Profile = () => {
+  return (
+    <>
+      <Header></Header>
+      <div className="form-container">
+        {/* Lock phần này, ko cho user sửa */}
+        <div className="details-container">
+          <h2 className="details-title">Your details</h2>
+          <form action="">
+            <div className="details-form-group">
+              <label for="">Full Name</label>
+              <input type="text" />
+            </div>
+
+            <div className="details-form-group">
+              <label for="">Phone Number</label>
+              <input type="text" />
+            </div>
+
+            <div className="details-form-group">
+              <label for="">Email</label>
+              <input type="text" />
+            </div>
+          </form>
+        </div>
+
+        <div className="details-container">
+          <h2 className="details-title">Tuition Information</h2>
+          <form action="">
+            <div className="details-form-group">
+              <label for="">Student ID</label>
+              <input type="text" />
+            </div>
+
+            <div className="details-form-group">
+              <label for="">Student Full Name</label>
+              <input type="text" />
+            </div>
+
+            <div className="details-form-group">
+              <label for="">Amount of tuition</label>
+              <input type="text" />
+            </div>
+          </form>
+        </div>
+
+        <div className="details-container">
+          <h2 className="details-title">Payment Information</h2>
+          <form action="">
+            <div className="details-form-group">
+              <label for="">Available Balance</label>
+              <input type="text" />
+            </div>
+
+            <div className="details-form-group">
+              <label for="">Amount of tuition</label>
+              <input type="text" />
+            </div>
+
+            {/* <!-- ??? Policy --> */}
+
+            {/* Button chỉ active khi info ở Tuition và Payment đc điền đủ */}
+            <button className="confirm-btn">Confirm</button>
+          </form>
+        </div>
+      </div>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default Profile;
