2fc31b78d7f6d96696082b253554cee84b0d645c
DannielVo <phattinhoc2017@gmail.com>
2025-09-13 10:54:29 +0700

    Feat: Add redirects file and and configure routes for Login and Profile pages

--- iBanking/src/App.jsx
+++ iBanking/src/App.jsx
@@ -3,12 +3,20 @@ import Login from "./pages/login/Login";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Profile from "./pages/profile/Profile";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Login></Login>
-      {/* <Profile></Profile> */}
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to="/login" replace></Navigate>}
+        ></Route>
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
     </>
   );
 };
